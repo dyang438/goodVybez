4085c5aa0727a0571847b701885189e8297171f2
dyang438 <152822883+dyang438@users.noreply.github.com>
2024-04-16 22:26:34 -0400

    continuing setup.

--- backend/index.ts
+++ backend/index.ts
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
